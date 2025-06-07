82cefe66678926ca492b44b16a52f3f2c0cea6a9
R0h@n <146733575+Rohan-Katyal@users.noreply.github.com>
2025-06-07 12:12:16 +0530

    Update main.js

--- Threejs-course-project-1/src/main.js
+++ Threejs-course-project-1/src/main.js
@@ -1,8 +1,8 @@
-// import * as THREE from 'three';
-import * as THREE from './node_modules/three/build/three.module.js';
-import {OrbitControls} from './node_modules/three/addons/controls/OrbitControls.js';
+// // import * as THREE from 'three';
+// import * as THREE from './node_modules/three/build/three.module.js';
+// import {OrbitControls} from './node_modules/three/addons/controls/OrbitControls.js';
 // initialising three.js => done
-console.log(THREE);
+// console.log(THREE);
 
 // Initialising the " SCENE "
 
